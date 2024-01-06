35c595eb9b8c128e7b8c5d34c6f482e9d7fedb19
Harleen <harlin7814@gmail.com>
2024-01-07 00:13:55 +0530

    first commit

---
+++ src/error-page.jsx
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError(); // When the user navigates to routes that dont exist
+  console.log(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpted error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
